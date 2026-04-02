d7fe09420ff0ad838e6ab2c4bcea8523de4b4d35
Rohit Raj <rajrohit7388@outlook.com>
2026-04-02 22:26:10 +0530

    Add files via upload

---
+++ MergeSort.java
@@ -0,0 +1,46 @@
+import java.util.*;
+class Mergesort
+{
+    public static void main(String args[])
+    {
+        Scanner sc = new Scanner(System.in);
+
+        int arr[] = {5,2,8,4,1,6,7,3,-5};
+        mergeSort(arr);
+        for(int ele : arr)
+            System.out.print(ele+" ");
+        sc.close();
+    }
+    private static void mergeSort(int []arr)
+    {
+        int n = arr.length;
+        if(n == 1)
+            return;
+        int[] a = new int[n/2];
+        int[] b = new int[n-n/2];
+        int idx = 0;
+        for(int i=0; i<a.length; i++)
+            a[i] = arr[idx++];
+        for(int i=0; i<b.length; i++)
+            b[i] = arr[idx++];
+
+        mergeSort(a);
+        mergeSort(b);
+        merge(a,b,arr);
+    }
+    public static void merge(int[] a, int[] b, int[] c)
+    {
+        int i=0, j=0, k=0;
+        while(i<a.length && j<b.length)
+        {
+            if(a[i]<=b[j]) 
+                c[k++] = a[i++];
+            else
+                c[k++] = b[j++];
+        }
+        while(i<a.length)
+            c[k++] = a[i++];
+        while(j<b.length)
+            c[k++] = b[j++];
+    }
+}
